98bbc4750ddb3c2c2a965161123d51a342b6c0a8
Елена Ла`врушева <law.lena.12@gmail.com>
2025-10-17 17:27:45 +0400

    Update inital_examination.py

--- notebooks/02_Initial_examination_and_data_preparation/inital_examination.py
+++ notebooks/02_Initial_examination_and_data_preparation/inital_examination.py
@@ -3,5 +3,5 @@ pd.set_option('display.max_columns', 100) # раскрыть все стольб
 pd.set_option('display.float_format', '{:.2f}'.format) # округлять до сотых
 df.info() # Общая информация
 df.size # Количество значений
-[ОПИСАНИЯ ПЕРЕМЕННЫХ](.notebooks/02_Initial_examination_and_data_preparation/variable_description.md)
+# ОПИСАНИЯ ПЕРЕМЕННЫХ в файле 
 df.describe() # Статистические сведения о датафрейме
